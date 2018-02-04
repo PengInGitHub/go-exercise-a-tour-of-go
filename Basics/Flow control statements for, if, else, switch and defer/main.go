bcd287af07d442e922259411353408b9e5b286cd
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-04 18:21:52 +0100

    add Flow Control-Exercise 3: for loop, is the same as 'while' loop in C

--- Basics/Flow control statements for, if, else, switch and defer/main.go
+++ Basics/Flow control statements for, if, else, switch and defer/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-//in for loop, the init and post statements are optional
+//"for" is the "while" in Go
 
 func main() {
 	sum := 1
-	for ; sum < 1000;  {
+	for sum < 1000 {
 		sum += sum
 	}
 	fmt.Println(sum)
