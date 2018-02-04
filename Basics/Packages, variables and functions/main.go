acfc5f6a12f18d415a4c855957eacdd5036e543b
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-04 16:55:02 +0100

    add exercise 13 of type conversion

--- Basics/Packages, variables and functions/main.go
+++ Basics/Packages, variables and functions/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+    "math"
 )
 
-//zero values:
-//variables are declared without explict initial values are given their zero values
-//outside a function, every statement starts with a keyword
+//type conversion:
+//the expression T(v) converts variable v to type T
+//i := 64
+//f := float64(i)
+//u := uint(f)
 
 func main() {
-    var a bool
-    var b string
-    var c int
-    var d float64
-	fmt.Printf("%v %q %v %v",a,b,c,d)
+    a, b := 3, 4 
+    c := math.Sqrt(float64(a*a+b*b))
+    d := uint(c)
+	fmt.Println(a,c,d)
 }
