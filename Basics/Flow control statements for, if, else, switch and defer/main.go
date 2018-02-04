a58696ec471e2bc2983dc2730a9046ecd0332e31
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-04 18:20:03 +0100

    add Flow Control-Exercise 2: for loop, init and post statements could be ommited

--- Basics/Flow control statements for, if, else, switch and defer/main.go
+++ Basics/Flow control statements for, if, else, switch and defer/main.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
-//to notem different from Java or C, there is no parentheses around the three components
-//of the for statement, and the braces are always required
+//in for loop, the init and post statements are optional
 
 func main() {
-	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
+	sum := 1
+	for ; sum < 1000;  {
+		sum += sum
 	}
 	fmt.Println(sum)
 }
