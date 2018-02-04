3404a9a9cb7e9fde9dd90183b2bb27d92b3e0d60
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-04 14:59:40 +0100

    add exercise on functions

--- Basics/Packages, variables and functions/main.go
+++ Basics/Packages, variables and functions/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func add(a int, b int) int{
+   return a+b
+}
+
 func main() {
-	fmt.Printf("Value of Pi: %g", math.Pi)
+	fmt.Printf("Value of 3+4: %v", add(3,4))
 }
