1e1068c6d2b0fa22ccc191bf4552111d7473cbac
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-04 18:14:01 +0100

    update Flow Control-Exercise 1: for loop

--- Basics/Flow control statements for, if, else, switch and defer/main.go
+++ Basics/Flow control statements for, if, else, switch and defer/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//to notem different from Java or C, there is no parentheses around the three components
+//of the for statement, and the braces are always required
+
 func main() {
 	sum := 0
 	for i := 0; i < 10; i++ {
