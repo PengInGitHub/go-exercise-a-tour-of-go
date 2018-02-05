0075c3afb34262814a6ae1ddf1019247bcbb8102
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-05 10:31:29 +0100

    refactor exercise 2: Imports, get more clear on how placeholder works

---
+++ Basics/Packages, variables and functions/Imports.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+    "fmt"
+    "math"
+)
+
+func PrintSqrt(n float64){
+    fmt.Printf("The sqrt of %v is %v", n, math.Sqrt(n))
+}
+
--- Basics/Packages, variables and functions/main.go
+++ Basics/Packages, variables and functions/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-    PrintRandomInt(12)
+    PrintSqrt(12)
 }
