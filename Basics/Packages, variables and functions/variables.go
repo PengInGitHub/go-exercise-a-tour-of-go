fe7a8e8466d98ef0f6accfc8b00d84cec87ccedc
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-05 12:13:00 +0100

    refactor exercise 8 variables: define variables via keyword var

---
+++ Basics/Packages, variables and functions/variables.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+"fmt"
+
+) 
+
+var dog, cat int
+var fish bool
+var parrot float64 
+
+func PrintVariables(){
+    var lizard bool
+    fmt.Println(dog, cat, fish, parrot, lizard )
+}
