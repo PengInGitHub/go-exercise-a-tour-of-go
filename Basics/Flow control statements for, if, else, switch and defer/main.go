9d0c51964fafc8201ff4283cac7fc9ba1cb8f5c2
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-05 13:25:00 +0100

    refactor main.go

--- Basics/Flow control statements for, if, else, switch and defer/main.go
+++ Basics/Flow control statements for, if, else, switch and defer/main.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-import "math"
-
-//if
-//if in Go is same like for, no () is required for statements but {} are required
-
-func sqrt(a float64) float64 {
-
-   if a<0 {
-        return sqrt(-a)
-    }
-    return math.Sqrt(a)
-}
-
 func main() {
-	fmt.Println(sqrt(-2), sqrt(16))
+
 }
