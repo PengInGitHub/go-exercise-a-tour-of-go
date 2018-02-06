2f4b8109497e308890517bcce689596f472356b0
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-06 14:17:04 +0100

    add exercise 13 creating a slice with make: how make works

--- Basics/More types structs, slices, and maps/making-slices.go
+++ Basics/More types structs, slices, and maps/making-slices.go
@@ -6,6 +6,10 @@ import (
 ) 
 //slices can be created by the built-in function make
 //this is how dynamically-sized arrays created
+
+//how make works: make allocates a zeroed array and returns a slice refers to the array
+//make has three parameters: array type, array length and array capacity
+
 func Print(){
     
     fmt.Println()
