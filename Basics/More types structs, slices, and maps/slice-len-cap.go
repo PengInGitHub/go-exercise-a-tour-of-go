d59163d355c8ad873381032fc5038f9e9eb2417d
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-06 13:45:15 +0100

    refactor exercise 11 slice length and capacity, slice the slice to give it zero length

--- Basics/More types structs, slices, and maps/slice-len-cap.go
+++ Basics/More types structs, slices, and maps/slice-len-cap.go
@@ -9,6 +9,12 @@ func PrintSliceLen(){
     SliceExample := []int{19,12,31,5,21,4}
     PrintSliceFunc(SliceExample)
 
+    //slice the slice to give it zero length
+    SliceExample = SliceExample[:0]
+    PrintSliceFunc(SliceExample)
+
+
+
 }
 
 
