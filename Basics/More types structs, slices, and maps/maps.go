b04512b7835e12c363cc1380ea31e1b90969800c
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-06 20:25:22 +0100

    add exercise 19 maps

--- Basics/More types structs, slices, and maps/maps.go
+++ Basics/More types structs, slices, and maps/maps.go
@@ -6,8 +6,16 @@ import (
 ) 
 //A map maps keys to values.
 
-func PrintMap(){
-    
-    fmt.Println()
+type Map2D struct{
+    lat, long float64
+}
 
+//make function returns a map of the given type
+
+var m map[string]Map2D
+
+func PrintMap(){
+    m = make(map[string]Map2D)
+    m["Berliner Dom"] = Map2D{52.0002, 12.32} 
+    fmt.Println(m["Berliner Dom"])
 }
