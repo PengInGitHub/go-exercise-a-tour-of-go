64edf1352804902d325eccdd58c0bd3bba30f36f
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-06 15:30:02 +0100

    add exercise 14: slice take slices, players take turns

--- Basics/More types structs, slices, and maps/slices-of-slice.go
+++ Basics/More types structs, slices, and maps/slices-of-slice.go
@@ -13,7 +13,14 @@ func PrintBoard(){
              []string{"_","_","_"},
              []string{"_","_","_"},
     }
-  
+
+    //players take turns
+    board[0][0]="x"   
+    board[2][2]="o"    
+    board[0][2]="x"    
+    board[1][0]="o"    
+    board[1][2]="x"    
+ 
     fmt.Println(board)
 
 }
