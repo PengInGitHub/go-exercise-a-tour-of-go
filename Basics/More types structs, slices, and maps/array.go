f1f2aadb4fe37421364a295bf53bf1d3f77c20c6
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-06 11:48:35 +0100

    add exercise 6 array

---
+++ Basics/More types structs, slices, and maps/array.go
@@ -0,0 +1,14 @@
+package main 
+
+import (
+"fmt"
+
+) 
+
+func PrintArray(){
+    var StringArray [1]string
+    StringArray[0] = "Ok"
+    a := [10]int{1,22,33,11,443,231,64,6643,31,21435}
+    fmt.Println(StringArray,a)
+
+}
