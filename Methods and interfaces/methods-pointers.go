f9f5c4a2243f414e9ef534741d0365e8ed54110b
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 19:54:08 +0100

    init exercise 4 methods pointer receiver

---
+++ Methods and interfaces/methods-pointers.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+"fmt"
+
+)
+
+//declare methods with pointer receivers
+//receiver has literal types *T
+//methods with pointer receiver could modify the value the receiver points 
+
+//pointer receivers are more common than value receivers, because methods often modify their receivers
+
+func PrintMethodsPointers(){
+    
+    
+    fmt.Println()
+}
