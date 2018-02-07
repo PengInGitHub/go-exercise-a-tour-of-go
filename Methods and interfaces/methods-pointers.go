80405e6764881e492e86274e0b253448c8081936
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 20:05:50 +0100

    finish exercise 4 methods pointer receiver

--- Methods and interfaces/methods-pointers.go
+++ Methods and interfaces/methods-pointers.go
@@ -11,8 +11,14 @@ import (
 
 //pointer receivers are more common than value receivers, because methods often modify their receivers
 
+func (r *RightTriangle) Scale(f float64){
+    r.SideA = r.SideA * f 
+    r.SideB = r.SideB * f 
+}
+
 func PrintMethodsPointers(){
     
-    
-    fmt.Println()
+    r := RightTriangle{300,400}
+    r.Scale(10)
+    fmt.Println(SideCLength(r))
 }
