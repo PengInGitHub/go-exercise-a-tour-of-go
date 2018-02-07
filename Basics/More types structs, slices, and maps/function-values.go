9436b5b240e11c7183b3c5488511ccf98b6c8223
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 12:44:51 +0100

    add exercise 23 closures, add comments for anonymous functions

--- Basics/More types structs, slices, and maps/function-values.go
+++ Basics/More types structs, slices, and maps/function-values.go
@@ -4,6 +4,8 @@ import (
 "fmt"
 
 ) 
+//Go supports anonymous functions, which can form closures.
+//Anonymous functions are useful when you want to define a function inline without having to name it.
 
 
 func PrintClosures(){
