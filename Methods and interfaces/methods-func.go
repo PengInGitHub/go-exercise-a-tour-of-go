68fcb2156aee04eb7e79e59b9598b73027d886b5
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 19:03:44 +0100

    finish exercise 2 methods functions

--- Methods and interfaces/methods-func.go
+++ Methods and interfaces/methods-func.go
@@ -2,11 +2,19 @@ package main
 
 import (
 "fmt"
-
+"math"
 )
 
+type RightTriangle struct{
+    SideA, SideB float64
+}
+
+func SideCLength(r RightTriangle) float64{
+    return math.Sqrt(r.SideA*r.SideA + r.SideB*r.SideB)
+}
+
 func PrintMethodFunc(){
-    
-    
-    fmt.Println()
+    r := RightTriangle{30,40}
+    //a method is just a function with a receiver argument
+    fmt.Println(SideCLength(r))
 }
