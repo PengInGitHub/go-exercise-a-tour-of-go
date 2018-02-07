215369139f1777f8bf14307a4b0ae99fb8c157ac
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 21:22:33 +0100

    finish exercise 5 pointers and functions

--- Methods and interfaces/methods-pointers-explained.go
+++ Methods and interfaces/methods-pointers-explained.go
@@ -2,20 +2,24 @@ package main
 
 import (
 "fmt"
-
+"math"
 )
 
 type AStruct struct{
     a, b float64
 }
 
-func ScaleFunc(s AStruct, f float64){
+func ScaleFunc(s *AStruct, f float64){
     s.a = s.a * f
     s.b = s.b * f
 }
 
+func AbsFunc(s AStruct) float64{
+    return math.Sqrt(s.a*s.a + s.b*s.b)
+}
+
 func PrintMethodsPointersEx(){
-    
-    
-    fmt.Println()
+    s := AStruct{3,4} 
+    ScaleFunc(&s,10)
+    fmt.Println(AbsFunc(s))
 }
