c50519e899b83d00f3f23f01b712ddc4e0a8f01c
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 09:34:38 +0100

    init exercise 22 mutating maps

---
+++ Basics/More types structs, slices, and maps/mutating-maps.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+"fmt"
+
+) 
+
+//why map?
+//One of the most useful data structures in computer science is the hash table. Many hash table implementations exist with varying 
+//properties, but in general they offer fast lookups, adds, and deletes.
+
+
+func Print(){
+    
+    fmt.Println()
+
+}
