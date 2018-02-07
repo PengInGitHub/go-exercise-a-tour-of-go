429b8113761a2fb4b96aabbd6f3193e477d9f1e8
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 09:05:22 +0100

    add exercise 20 map literals

--- Basics/More types structs, slices, and maps/maps-literals.go
+++ Basics/More types structs, slices, and maps/maps-literals.go
@@ -5,8 +5,21 @@ import (
 
 ) 
 //Map literals are like struct literals, but the keys are required.
+
+type MapLatLong struct{
+    lat, long float64
+}
+
 func PrintMapLiterals(){
+    var m = map[string]MapLatLong{
+        "Friedrichstr":MapLatLong{
+            53.2,11.2, //why one more comma after two numbers?
+        },
+        "Gesundbounen":MapLatLong{
+            54.22,10.2,
+        },
+    }
     
-    fmt.Println()
+    fmt.Println(m)
 
 }
