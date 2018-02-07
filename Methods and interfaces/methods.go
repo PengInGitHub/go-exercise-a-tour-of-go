d471a1d84a12a0f45174c3e34b978ee8334550bc
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 18:38:15 +0100

    add exercise 1 methods, add definition of method

--- Methods and interfaces/methods.go
+++ Methods and interfaces/methods.go
@@ -13,6 +13,9 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+//method in Go is the same as method in OOP, which belongs to a class
+//and could be called only from an instance of the class
+
 func PrintMethod(){
     v := Vertex{3, 4}
 	fmt.Println(v.Abs())
