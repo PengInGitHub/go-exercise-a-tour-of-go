f887c4607110af710f91275796384aab31f5dd39
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 09:35:32 +0100

    init exercise 22 mutating maps, add comments of why and how to use map

--- Basics/More types structs, slices, and maps/mutating-maps.go
+++ Basics/More types structs, slices, and maps/mutating-maps.go
@@ -9,6 +9,9 @@ import (
 //One of the most useful data structures in computer science is the hash table. Many hash table implementations exist with varying 
 //properties, but in general they offer fast lookups, adds, and deletes.
 
+//map in golang
+//Go provides a built-in map type that implements a hash table.
+
 
 func Print(){
     
