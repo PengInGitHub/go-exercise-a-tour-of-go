b3d5a587f4e1dd5dc500ce1457ce6e673eda0601
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 19:26:17 +0100

    finish exercise 3 methods continued

--- Methods and interfaces/methods-continued.go
+++ Methods and interfaces/methods-continued.go
@@ -7,10 +7,16 @@ import (
 
 //declare a method on non-struct type
 
+type MyFloat float64
 
+func (m MyFloat) Abs() float64{
+    if m< 0 {
+        return float64(-m)
+    }
+    return float64(m)
+}
 
 func PrintMyFloat(){
-    
-    
-    fmt.Println()
+    var m MyFloat = -64   
+    fmt.Println(m.Abs())
 }
