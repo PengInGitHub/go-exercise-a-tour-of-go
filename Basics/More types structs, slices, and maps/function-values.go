73a04ba41f1d856d5531755fca025d2fa5e617a4
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-07 12:47:18 +0100

    add exercise 23 closures, add comments to define closures

--- Basics/More types structs, slices, and maps/function-values.go
+++ Basics/More types structs, slices, and maps/function-values.go
@@ -7,6 +7,9 @@ import (
 //Go supports anonymous functions, which can form closures.
 //Anonymous functions are useful when you want to define a function inline without having to name it.
 
+//define closures
+//A closure is a function value that references variables from outside its body.
+//The function may access and assign to the referenced variables
 
 func PrintClosures(){
     
