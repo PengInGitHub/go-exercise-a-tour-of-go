693a84b59f1085893ecceb9575282dac1faa5354
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 08:21:26 +0100

    add methods pointer indirection, init VertextIndirection

--- Methods and interfaces/indirection-values.go
+++ Methods and interfaces/indirection-values.go
@@ -2,11 +2,19 @@ package main
 
 import (
 "fmt"
-
+"math"
 )
 
+func (v *VertextIndirection) AbsIn() float64{
+    return math.Sqrt(v.a*v.a + v.b*v.b)
+}
+
+func AbsInFunc(v *VertextIndirection) float64{
+    return math.Sqrt(v.a*v.a + v.b*v.b)
+}
+
 func PrintIndirectionValues(){
+    v := VertextIndirection{3,4}
     
-    
-    fmt.Println()
+    fmt.Println(v.AbsIn())
 }
