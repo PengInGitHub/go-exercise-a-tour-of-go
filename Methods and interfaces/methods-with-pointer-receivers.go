855f79987bd4495637f5d21483cf0d06d4873649
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 08:40:32 +0100

    finish exercise 8 choosing a value or pointer receiver, add comments on the reasons to use pointer: 1.modify values, 2.avoid copying data

--- Methods and interfaces/methods-with-pointer-receivers.go
+++ Methods and interfaces/methods-with-pointer-receivers.go
@@ -5,8 +5,12 @@ import (
 
 )
 
+//reasons to choose pointer receiver
+//1.methods can modify the value the receiver points to
+//2.avoid copying value in each method call, especially efficient when the receiver is a large struct
+
+
 func Print(){
-    
-    
-    fmt.Println()
+
+    fmt.Printf("a")
 }
