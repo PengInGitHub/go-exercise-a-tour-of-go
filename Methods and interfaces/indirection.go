500050b5e4c5194ad76975d9e4f07841efcf470f
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-08 07:45:59 +0100

    init exercise 6 methods and pointer indirection, add comments for methods with pointer receiver take both pointer and value

--- Methods and interfaces/indirection.go
+++ Methods and interfaces/indirection.go
@@ -7,6 +7,12 @@ import (
 //functions with a pointer argument must take a pointer
 //var v Vertex
 //ScaleFunc(&v, 10)
+
+//in comparison methods with pointer receiver take either a pointer or a value
+//var v Vertex
+//&v.(10) --- this is ok
+//v.(10) --- this is fine as well
+
 func PrintIndirection(){
     
     
