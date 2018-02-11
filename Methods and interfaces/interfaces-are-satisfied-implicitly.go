deb8d38971a2d17281429db288028d6ae7ceb49d
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:34:40 +0100

    implement methods of interfaces

--- Methods and interfaces/interfaces-are-satisfied-implicitly.go
+++ Methods and interfaces/interfaces-are-satisfied-implicitly.go
@@ -7,7 +7,7 @@ import (
 
 //how to implement interface
 //There is no explicit declaration of intent, no "implements" keyword.
-
+//A type implements an interface by implementing its methods. 
 
 func Print(){
     
