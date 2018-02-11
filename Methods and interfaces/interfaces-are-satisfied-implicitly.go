2fa2a44efec59996f82be445a0d546070d3b080d
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:49:26 +0100

    add interface I, type T, let T implements I since t is the receiver of method M that belongs to I

--- Methods and interfaces/interfaces-are-satisfied-implicitly.go
+++ Methods and interfaces/interfaces-are-satisfied-implicitly.go
@@ -13,6 +13,20 @@ import (
 //Implicit interfaces decouple the definition of an interface from its implementation
 //which could then appear in any package without prearrangement.
 
+type I interface{
+     M()
+}
+
+type T struct{
+    S string
+}
+
+//type T implements the interface I
+func (t T) M(){
+    fmt.Println(t.S)
+}
+
+
 func Print(){
     
     
