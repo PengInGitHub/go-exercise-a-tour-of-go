f3b2b6b304071b025ffff62b6b4e8e68eec0db53
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:55:44 +0100

    add explaination of interface values

---
+++ Methods and interfaces/interface-values.go
@@ -0,0 +1,14 @@
+package main 
+
+import (
+"fmt"
+
+)
+//interface values can be thought of as a tuple of a value and a concrete type
+//(value,type)
+
+func Print(){
+    
+    
+    fmt.Println()
+}
