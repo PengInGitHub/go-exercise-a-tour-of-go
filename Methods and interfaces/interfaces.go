0364957279c2ace64b990304810a0b5e1aace4d8
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:29:57 +0100

    print a.Abs()

--- Methods and interfaces/interfaces.go
+++ Methods and interfaces/interfaces.go
@@ -14,11 +14,11 @@ type Abser interface{
     Abs() float64
 }
 
-type MyFloat float64
+type MyFloat2 float64
 
-func (f MyFloat) Abs float64{
+func (f MyFloat2) Abs() float64{
     if f<0 {
-        float64(-f)
+       return float64(-f)
     }
     return float64(f)
 }
@@ -27,18 +27,18 @@ type VertexInterface struct{
     X, Y float64
 }
 
-func (v *VertexInterface) Abs float64{
+func (v *VertexInterface) Abs() float64{
     return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func PriPrintInterfacent(){
+func PrintInterface(){
     var a Abser
 
-    f := MyFloat(-math.Sqrt2)
+    f := MyFloat2(-math.Sqrt2)
     v := VertexInterface{3,4}
 
-    a = f //a MyFloat implements Abser
+    a = f //a MyFloat2 implements Abser
     a = &v //a My*VertexInterfaceFloat implements Abser
     
-    fmt.Println()
+    fmt.Println(a.Abs())
 }
