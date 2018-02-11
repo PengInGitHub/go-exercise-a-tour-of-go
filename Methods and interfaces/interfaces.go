03aa8b2bf8697dba350a6888b153786f057f9d63
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:17:54 +0100

    add interface, method Abs() with return type float64

--- Methods and interfaces/interfaces.go
+++ Methods and interfaces/interfaces.go
@@ -2,12 +2,17 @@ package main
 
 import (
 "fmt"
-
+"math"
 )
 
 //define interface
 //An interface type is defined as a set of method signatures.
 
+//A value of interface type can hold any value that implements those methods.
+
+type Abser interface{
+    Abs() float64
+}
 
 func PriPrintInterfacent(){
     
