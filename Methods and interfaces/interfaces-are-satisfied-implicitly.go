58d7100da6a6441e3f2ce5657ac5a36d9c991518
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-11 18:38:42 +0100

    add reasons for implict interfaces

--- Methods and interfaces/interfaces-are-satisfied-implicitly.go
+++ Methods and interfaces/interfaces-are-satisfied-implicitly.go
@@ -9,6 +9,10 @@ import (
 //There is no explicit declaration of intent, no "implements" keyword.
 //A type implements an interface by implementing its methods. 
 
+//why implicit interfaces
+//Implicit interfaces decouple the definition of an interface from its implementation
+//which could then appear in any package without prearrangement.
+
 func Print(){
     
     
