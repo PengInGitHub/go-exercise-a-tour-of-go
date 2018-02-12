0252abae71488f4bc927935d13c0e97cf214a339
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-12 20:32:42 +0100

    add type  of interface values

--- Methods and interfaces/interface-values.go
+++ Methods and interfaces/interface-values.go
@@ -7,6 +7,16 @@ import (
 //interface values can be thought of as a tuple of a value and a concrete type
 //(value,type)
 
+//Calling a method on an interface value executes the method of the same name on its underlying type.
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+
 func Print(){
     
     
